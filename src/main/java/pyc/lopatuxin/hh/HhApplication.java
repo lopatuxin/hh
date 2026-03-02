59b5ae3e8d22cd2c8b2d5b990845d27913b8a5cd
lopatuxin <lopatuxin.a@gmail.com>
2026-03-02 20:47:07 +0300

    init commit

---
+++ src/main/java/pyc/lopatuxin/hh/HhApplication.java
@@ -0,0 +1,13 @@
+package pyc.lopatuxin.hh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pyc/lopatuxin/hh/HhApplicationTests.java
@@ -0,0 +1,13 @@
+package pyc.lopatuxin.hh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
