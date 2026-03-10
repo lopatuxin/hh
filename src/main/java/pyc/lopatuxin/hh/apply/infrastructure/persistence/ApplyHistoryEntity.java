b9033291f6ca3a23aa7bf9be3c3c64ffcf3b412a
lopatuxin <lopatuxin.a@gmail.com>
2026-03-10 13:37:51 +0300

    Добавил JPA-сущность ApplyHistoryEntity

---
+++ src/main/java/pyc/lopatuxin/hh/apply/infrastructure/persistence/ApplyHistoryEntity.java
@@ -0,0 +1,24 @@
+package pyc.lopatuxin.hh.apply.infrastructure.persistence;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "applied_vacancies")
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApplyHistoryEntity {
+
+    @Id
+    @Column(name = "vacancy_id")
+    private String vacancyId;
+
+    @Column(name = "applied_at", nullable = false)
+    private Instant appliedAt;
+}
