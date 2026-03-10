bd80f7166719e1b8c991c37e03a48145a264eec8
lopatuxin <lopatuxin.a@gmail.com>
2026-03-10 13:40:29 +0300

    Добавил Spring Data репозиторий ApplyHistoryRepository

---
+++ src/main/java/pyc/lopatuxin/hh/apply/infrastructure/persistence/ApplyHistoryRepository.java
@@ -0,0 +1,6 @@
+package pyc.lopatuxin.hh.apply.infrastructure.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ApplyHistoryRepository extends JpaRepository<ApplyHistoryEntity, String> {
+}
